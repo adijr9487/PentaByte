cf384b9b493e1873f34436b966367814da72a2bf
Divyans Falodiya <divyanshfalodiyaab@gmail.com>
2022-05-07 19:47:38 +0530

    :sparkls: added dbscan

--- Routers/home.js
+++ Routers/home.js
@@ -24,6 +24,32 @@ router.post("/bins/fetch", async (req, res) => {
     }
 });
 
+// Fetch bins for user
+router.post(
+    "/bins/user/fetch",
+    authController.isAuthenticated,
+    async (req, res) => {
+        try {
+            if (!req.body.zip)
+                return errorHandler.handleBadRequest(res, "Invalid Zip Code");
+
+            const admin = await Admin.findOne({ zip: req.body.zip });
+            if (!admin)
+                return errorHandler.handleNotFound(
+                    res,
+                    "No data found for the requested zip code."
+                );
+
+            const bins = await Bins.find({ admin: admin._id }).select(
+                "location"
+            );
+            res.status(200).json({ locations: bins.location });
+        } catch (e) {
+            errorHandler.handleInternalServer(res);
+        }
+    }
+);
+
 // Update bins for admin
 router.post("/bins", authController.isAuthenticatedAdmin, async (req, res) => {
     try {
